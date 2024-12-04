5014e32beee2ac334d086d0de4f414e3f835da05
Arjun Syam <arsyam@cisco.com>
2024-12-05 00:55:01 +0530

    fixed css

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Toolbar from "./components/Toolbar";
 
 const App = () => {
 
-  const [pageState, setPageState] = useState("inactive");
+  const [previewState, setpreviewState] = useState("inactive");
 
-  const handlePageStateChange = (e) => {
-    setPageState(e.target.value);
+  const handlepreviewStateChange = (e) => {
+    setpreviewState(e.target.value);
   };
 
   return (
@@ -18,12 +18,12 @@ const App = () => {
       </div>
       <div className="tool-bar-container">
         <Toolbar 
-          handlePageStateChange={handlePageStateChange}
+          handlepreviewStateChange={handlepreviewStateChange}
         />
       </div>
       <div className="preview-panel-container">
         <PreviewPanel  
-          state={pageState}
+          state={previewState}
         />
       </div>
       <div className="right-panel-container">
---
+++ src/components/PreviewPanel/InteractionControl/InteractionControl.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const renderBlock = () => {
+    return (
+        <div>
+            <h1>Interaction Block</h1>
+            <p>This is a dummy component for the Interaction Block.</p>
+        </div>
+    );
+};
+const InteractionBlock = () => {
+    return renderBlock();
+};
+
+export default InteractionBlock;
