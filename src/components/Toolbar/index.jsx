e2c16a61a3d9e72a6f81b43bea109070f1ec448a
“aatray” <aatray@cisco.com>
2024-12-05 15:57:17 +0530

    Minor fix

--- src/components/Toolbar/index.jsx
+++ src/components/Toolbar/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch } from 'react-redux';
-import { setPreviewState } from '../store/previewStateSlice';
+import { setPreviewState } from "../../store/previewStateSlice";
 import './Toolbar.css';
 
 const Toolbar = () => {
