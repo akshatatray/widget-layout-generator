517d27283d916c328ddcfc449b46e9d60f6b479d
Arjun Syam <arsyam@cisco.com>
2024-12-05 12:15:17 +0530

    fixed css and added panels

---
+++ src/components/PreviewPanel/Panels/WidgetPanel/WidgetPanel.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WidgetPanel = () => {
+    return (<>
+        <div>
+            <h1>widget panels</h1>
+        </div>
+    </>
+    );
+};
+
+export default WidgetPanel;
