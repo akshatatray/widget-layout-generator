491f6efcf83462c683804557edc55dbb95fc0f9d
misriniv <misriniv@cisco.com>
2024-12-08 16:44:54 +0530

    Add new component

---
+++ src/components/RightPanel/WidgetList/SortableItem.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import "./WidgetList.css";
+import {
+    SortableContext,
+    arrayMove,
+    verticalListSortingStrategy,
+  } from "@dnd-kit/sortable";
+
+function SortableItem({ id, item, setEditAWidget, setEditKey, setItems, items }) {
+    const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+  return (
+    <div
+      className="widget-list-item"
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...(item.isDraggable ? listeners : {})}
+    >
+      <div className="draggable-icon">
+        <md-icon
+          iconSet="preferMomentumDesign"
+          name="more-adr_20"
+          color={item.isDraggable ? "#000000" : "#999999"}
+        />
+      </div>
+      <div className="widget-details">
+        <md-icon iconSet="preferMomentumDesign" name={item.icon} />
+        <p>{item.name}</p>
+      </div>
+      <div className="customise-icons">
+        <div className="edit-container">
+          <md-icon
+            iconSet="preferMomentumDesign"
+            name="edit_16"
+            color={item.isEditable ? "#000000" : "#999999"}
+            onClick={() => {
+              if (item.isEditable) {
+                setEditKey(id);
+                setEditAWidget(true);
+              }
+            }}
+          />
+        </div>
+        <div className="delete-container">
+          <md-icon
+            onClick={() => {
+              if (item.isDeletable) {
+                const newListItems = items.filter(data => data.id != id);
+                setItems(newListItems)
+              }
+            }}
+            iconSet="preferMomentumDesign"
+            name="delete_16"
+            color={item.isDeletable ? "#000000" : "#999999"}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SortableItem
