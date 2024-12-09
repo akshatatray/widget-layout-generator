b8450c751d1c56b7cdaecf4c9668f46458fac5ca
misriniv <misriniv@cisco.com>
2024-12-09 14:02:32 +0530

    header right reordered

--- src/store/initial-constants/initialRightHeaderLayout.js
+++ src/store/initial-constants/initialRightHeaderLayout.js
@@ -1,22 +1,20 @@
 export const initialRightHeaderLayout = [
     {
         id: 1,
-        type: 'circle-button',
-        name: 'webex',
-        iconName: 'webex_16',
-        label: 'Webex',
-        iconSet: "preferMomentumDesign",
-        isDraggable: true,
+        type: 'avatar',
+        name: 'profile',
+        title: 'Widget Craft',
+        label: 'Profile',
+        isDraggable: false,
         isEditable: true,
-        isDeletable: true
+        isDeletable: false
     },
     {
         id: 2,
-        type: 'circle-button',
-        name: 'outdial',
-        iconName: 'audio-call_16',
-        label: 'Outdial',
-        iconSet: "preferMomentumDesign",
+        type: 'state-button',
+        name: 'stateSelector',
+        variant: 'available',
+        label: 'Available',
         isDraggable: true,
         isEditable: true,
         isDeletable: true
@@ -34,22 +32,24 @@ export const initialRightHeaderLayout = [
     },
     {
         id: 4,
-        type: 'state-button',
-        name: 'stateSelector',
-        variant: 'available',
-        label: 'Available',
+        type: 'circle-button',
+        name: 'outdial',
+        iconName: 'audio-call_16',
+        label: 'Outdial',
+        iconSet: "preferMomentumDesign",
         isDraggable: true,
         isEditable: true,
         isDeletable: true
     },
     {
         id: 5,
-        type: 'avatar',
-        name: 'profile',
-        title: 'Widget Craft',
-        label: 'Profile',
-        isDraggable: false,
+        type: 'circle-button',
+        name: 'webex',
+        iconName: 'webex_16',
+        label: 'Webex',
+        iconSet: "preferMomentumDesign",
+        isDraggable: true,
         isEditable: true,
-        isDeletable: false
+        isDeletable: true
     },
 ]
