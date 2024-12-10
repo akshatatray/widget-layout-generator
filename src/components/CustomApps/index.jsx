201091d3380e709558c5fa90b028ac5cf521f0e7
“aatray” <aatray@cisco.com>
2024-12-10 21:57:14 +0530

    Added few widgets.

--- src/components/CustomApps/index.jsx
+++ src/components/CustomApps/index.jsx
@@ -1,7 +1,5 @@
 import TimeWidget from "./TimeWidget";
 import WeatherWidget from "./WeatherWidget";
-import NewsWidget from "./NewsWidget";
-import QuoteWidget from "./QuoteWidget";
 import MapsWidget from "./MapsWidget";
 
 export const AppLibrary = () => {
@@ -9,8 +7,6 @@ export const AppLibrary = () => {
         blocks: {
             Time: TimeWidget({}),
             Weather: WeatherWidget({}),
-            News: NewsWidget({}),
-            Quote: QuoteWidget({}),
             Maps: MapsWidget({}),
         }
     }
